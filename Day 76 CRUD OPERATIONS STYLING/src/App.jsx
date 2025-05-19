ec4f5a3efad41d2891d7b8caf615427e7a9faf24
Adnan Meman <memanmmoadnan5259@gmail.com>
2025-05-19 10:24:58 +0530

    Todo in React

--- Day 76 CRUD OPERATIONS STYLING/src/App.jsx
+++ Day 76 CRUD OPERATIONS STYLING/src/App.jsx
@@ -1,24 +1,44 @@
 import React, { use } from "react";
 import { useState } from "react";
+import { nanoid } from "nanoid"; // ✅ Import nanoid
+
 const App = () => {
-  const [todo, settodo] = useState([
+  const [todos, settodo] = useState([
     { id: 1, title: "Kuch kaelo", isCompleted: false },
   ]);
   const [title, settitle] = useState("");
-  const [completed, setcompleted] = useState(true);
-  const [gender, setgender] = useState("Male");
-  const [city, setcity] = useState("Chennai");
+  // const [completed, setcompleted] = useState(true);
+  // const [gender, setgender] = useState("Male");
+  // const [city, setcity] = useState("Chennai");
+  const SubmitHandeler = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: nanoid(),
+      title: title,
+      isCompleted: false,
+    };
+
+    let copytodos = [...todos];// Create a copy of the existing todos
+    copytodos.push(newTodo);// Add the new todo to the copy
+    settodo(copytodos);// Update the state with the new copy
+
+    // settodo([...todo, newTodo]); // Update the state with the new todo
+  };
+
+  const renderTodos = todos.map((todo) => {
+    return <li key={todo.id}>{todo.title}</li>
+  })
   return (
     <div>
       <h1>Todo App</h1>
-      <form>
+      <form onSubmit={SubmitHandeler}>
         <input
+          type="text"
           onChange={(event) => settitle(event.target.value)}
           value={title}
-          type="text"
           placeholder="Add a new todo"
         />
-        <br />
+        {/*  <br />
         <br />
         <input
           checked={completed}
@@ -52,11 +72,17 @@ const App = () => {
           <option value="Banglore">Banglore</option>
           <option value="Chennai">Chennai</option>
           <option value="Kolkata">Kolkata</option>
-        </select>
+        </select>*/}
         <br />
         <br />
         <button type="submit">Add</button>
       </form>
+      <hr />
+      <h2>Todo List</h2>
+      <ul>
+        {renderTodos}
+       
+       </ul>
     </div>
   );
 };
